9bd4ac54e713b5569778bb5c377aaee8606a49c3
Jean Fondeur <jean@odsoluciones.com>
2018-09-29 18:47:13 -0400

    cta

--- include/footercta_pt.php
+++ include/footercta_pt.php
@@ -1,11 +1,31 @@
+<?php
+$currentlang = apply_filters( 'wpml_current_language', NULL );
+?>
 <section class="getStartedModule bg-light">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-8 text-center">
-                    <h2 class="brown">Solicita uma chamada com um consultor hoje</h2>
-                    <p>Nossos consultores apoiam investidores latino-americanos em espanhol e português</p>
+                    <h2 class="brown">
+                    <?php
+                        if($currentlang == 'pt-br'){
+                            echo 'Solicita uma chamada com um consultor hoje';
+                        }elseif($currentlang == 'es'){
+                            echo 'Solicite una llamada con un consultor hoy';
+                        }
+                    ?>
+                    </h2>
+                    <p><?php
+                        if($currentlang == 'pt-br'){
+                            echo 'Nossos consultores apoiam investidores latino-americanos em espanhol e português';
+                        }elseif($currentlang == 'es'){
+                            echo 'Nuestros consultores apoyan los inversionistas de América Latina en español y portugués';
+                        }
+                    ?></p>
                     <?php get_template_part('include/optin')?>
                 </div>
             </div>
         </div>
     </section>
+
+    Nuestros consultores apoyan los inversores de América Latina en español y portugués
+    Nossos consultores apoiam investidores latino-americanos em espanhol e português
