64e679c585a75b31c29642937a80989ec2148b67
Jean Fondeur <jean@odsoluciones.com>
2018-09-29 18:29:51 -0400

    hero

--- include/hero.php
+++ include/hero.php
@@ -2,7 +2,7 @@
     <section class="baiHero homeHero h-50 bg-light" style="background:url('<?php the_field('hero_background')?>');  background-size:cover;<?php if(is_page(102)): echo 'background-position:bottom !important'; endif?>">
         <div class="container">
             <div class="row align-items-center h-100">
-                <div class="col-md-5">
+                <div class="col-md-6">
                     <h2 class=""><?php the_field('hero_heading') ?></h2>
                     <p><?php the_field('hero_text') ?></p>
                     <?php get_template_part('include/optin')?>
