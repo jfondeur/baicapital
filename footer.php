710bb8eceb714737300cc208811abae8d904811a
Jean Fondeur <jean@odsoluciones.com>
2018-11-14 09:23:22 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -9,17 +9,17 @@
 					<div class="footer-2">
 						<ul class="list-unstyled">
 						<?php if($currentlang == 'pt-br'): ?>
-							<li class="mb-2"><a href="/sobrebaicapital/">Sobre BAI Capital</a></li>
-							<li class="mb-2"><a href="/projetos-eb5/">Projetos EB5</a></li>
-							<li class="mb-2 not-us"><a href="/projetos-equity/">Projetos Equity</a></li>
+							<li class="mb-2"><a href="/?page_id=8">Sobre BAI Capital</a></li>
+							<li class="mb-2"><a href="//?page_id=85">Projetos EB5</a></li>
+							<li class="mb-2 not-us"><a href="//?page_id=108">Projetos Equity</a></li>
 						<?php elseif($currentlang == 'es'): ?>
-							<li class="mb-2"><a href="/sobrebaicapital/">Sobre BAI Capital</a></li>
-							<li class="mb-2"><a href="/projetos-eb5/">Proyetos EB5</a></li>
-							<li class="mb-2 not-us"><a href="/projetos-equity/">Proyetos Equity</a></li>
+							<li class="mb-2"><a href="/?page_id=323">Sobre BAI Capital</a></li>
+							<li class="mb-2"><a href="/?page_id=353">Proyetos EB5</a></li>
+							<li class="mb-2 not-us"><a href="/?page_id=354">Proyetos Equity</a></li>
 						<?php elseif($currentlang == 'en'): ?>
-							<li class="mb-2"><a href="/sobrebaicapital/">About BAI Capital</a></li>
-							<li class="mb-2"><a href="/projetos-eb5/">EB5 Projects</a></li>
-							<li class="mb-2 not-us"><a href="/projetos-equity/">Equity projects</a></li>
+							<li class="mb-2"><a href="/?page_id=475">About BAI Capital</a></li>
+							<li class="mb-2"><a href="/?page_id=489">EB5 Projects</a></li>
+							<li class="mb-2 not-us"><a href="/?page_id=491">Equity projects</a></li>
 						<?php endif;?>
 						</ul>
 					</div>
