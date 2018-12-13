ed6e8dc08a20ec58d3bb1e040ea03d02a958c6b4
Jean Fondeur <jean@odsoluciones.com>
2018-12-13 16:39:12 -0400

    Update single.php

--- single.php
+++ single.php
@@ -26,7 +26,7 @@ global $currentlang;
                         <?php the_content();?>
                     </div>
                     <div class="col-md-4 sidebar">
-                        <div class="sidebarCta border rounded d-none">
+                        <div class="sidebarCta border rounded">
                             <img src="https://baicapital.com/wp-content/uploads/2018/09/rawpixel-743067-unsplash-1.jpg" alt="">
                             <div class="p-2">
                                 <span class="brown"><strong>Newsletter</strong></span>
@@ -43,7 +43,7 @@ global $currentlang;
                             </div>
                         </div>
                         
-						<p class="brown">
+						<p class="brown mt-5">
                             <strong>
                             <?php if($currentlang=='es'){
                                 echo 'Articulos relacionados';
