985742a9549b530fa82fd9f7dd25ee290d1da383
jfondeur <jfondeur@users.noreply.github.com>
2020-03-11 09:28:54 -0400

    Revert "single projects"
    This reverts commit 3f6b979c6a8b2c8769c8629ada03b12c132e78ec.

--- single-projects.php
+++ single-projects.php
@@ -41,10 +41,8 @@ global $currentlang;
                                 <p><?php the_field('location_text') ?></p>
                                 <p class="strong">Developer</p>
                                 <p><?php the_field('developer') ?></p>
-                                <?php if (get_the_field('general_partner_of_eb5_investors')): ?>
-                                    <p class="strong">General partner of EB5 investors</p>
-                                    <p><?php the_field('general_partner_of_eb5_investors') ?></p>
-                                <?php endif; ?>
+                                <p class="strong">General partner of EB5 investors</p>
+                                <p><?php the_field('general_partner_of_eb5_investors') ?></p>
                                 <p class="strong">Regional Center</p>
                                 <p><?php the_field('regional_center') ?></p>
                             </div>
