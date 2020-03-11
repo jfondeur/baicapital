d98e7aff5bc5ff8acc6eef7bb354b69f115a8b78
jfondeur <jfondeur@users.noreply.github.com>
2020-03-11 09:43:18 -0400

    Update single-projects.php

--- single-projects.php
+++ single-projects.php
@@ -51,11 +51,11 @@ global $currentlang;
                             <div class="col-md-6">
                                 <p class="strong">Counselor Attorney</p>
                                 <p><?php the_field('counselor_attorney') ?></p>
-                                <p class="strong">EB5 Capital Raise</p>
-                                <p>$<?php the_field('eb5_capital_raise') ?></p>
                                 <?php if ($pc[0]->name == 'EB5') :?>
-                                        <p class="strong">Total Project Capital</p>
-                                        <p>$<?php the_field('total_project_capital') ?></p>
+                                    <p class="strong">EB5 Capital Raise</p>
+                                    <p>$<?php the_field('eb5_capital_raise') ?></p>
+                                    <p class="strong">Total Project Capital</p>
+                                    <p>$<?php the_field('total_project_capital') ?></p>
                                 <?php endif;?>
                                 <?php if(get_field('total_jobs_impact')):?>
                                     <p class="strong">Total Jobs Impact</p>
