0f2176a75499267e58e50218547fda99847e3da5
jfondeur <jfondeur@users.noreply.github.com>
2020-03-24 16:20:43 -0400

    single video title projects

--- single-projects.php
+++ single-projects.php
@@ -140,7 +140,7 @@ global $currentlang;
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Videos</h5>
+                                <h5 class="modal-title" id="exampleModalLabel">Videos - <?php the_title();?></h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                                 </button>
