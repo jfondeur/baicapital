a64e43f6207b094255cea640c883d182e4643dad
jfondeur <jfondeur@users.noreply.github.com>
2020-04-23 22:14:04 -0400

    Update page-home.php

--- page-home.php
+++ page-home.php
@@ -7,6 +7,7 @@ get_template_part('include/hero');
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
+                    <!-- heading -->
                     <h4 class="brown mb-4"><?php
                         if($currentlang=='es'){
                            echo 'Hemos sido reseñados en:';
